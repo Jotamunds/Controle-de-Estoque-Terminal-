d23ea640e6907c63dea3dde0be6563a654186404
João Gabriel <joaogabrielvs2022@gmail.com>
2024-11-26 23:41:52 -0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -9,7 +9,7 @@ def escolhas():
         # Variáveis
         caminho_banco = r'controle_de_estoque.db'
         conexao = sqlite3.connect(caminho_banco)
-        conexao.execute("PRAGMA journal_mode=WAL;") # não cria o arquivo db-journal
+        conexao.execute("PRAGMA journal_mode=WAL;")
         cursor = conexao.cursor()
 
         # Sistema
@@ -35,7 +35,7 @@ def escolhas():
                 editar_dados(cursor, conexao)
             case 3:
                 id = int(input("ID do produto: "))
-                cursor.execute(f"DELETE FROM Produtos WHERE id = ?", (id,)) # quando for passar um único valor deve ser colocada a vírgula para o python entender que é uma tupla com valores
+                cursor.execute(f"DELETE FROM Produtos WHERE id = ?", (id,))
                 conexao.commit()
                 print("Produto removido com sucesso!")
                 print()
@@ -82,7 +82,7 @@ def mostrar_dados(cursor, conexao):
     produtos = cursor.fetchall()
 
     # Tabela
-    print(f"{'ID':<5} {'Produto':<30} {'Preço':<10}") # :<20 deixa os elementos no canto esquerdo da tela com 20 de distância
+    print(f"{'ID':<5} {'Produto':<30} {'Preço':<10}")
     print("-" * 40)
 
     # Exibir os dados
